ce487a4577b195922c89e472e4a32e1c8b35a64a
Mihriban Minaz <mihri@unumotors.com>
2018-11-12 18:03:09 +0100

    Add test works for 0.5 + 0.6

--- test/addition.js
+++ test/addition.js
@@ -25,6 +25,12 @@ test('Add 0.1 + 0.2', t => {
     t.is(result, 0.3, 'Output should be equal to 0.3')
   })
 
+  test('Add 0.5 + 0.6', t => {
+    const result = library.add(0.5, 0.6)
+
+    t.is(result, 1.1, 'Output should be equal to 1.1')
+  })
+
 // test('automatic pass', t => t.pass())
 // test('automatic pass', t => t.fail())
 
