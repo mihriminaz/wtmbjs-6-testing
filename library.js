71eb8e02242b041803defb76b4b40cbb5503d0ec
Mihriban Minaz <mihri@unumotors.com>
2018-11-12 17:54:03 +0100

    Addition test with simple function

---
+++ library.js
@@ -0,0 +1,1 @@
+exports.add = () => 8
--- test/addition.js
+++ test/addition.js
@@ -1,4 +1,12 @@
 import test from 'ava'
+import library from '../library'
 
-test('automatic pass', t => t.pass())
-test('automatic pass', t => t.fail())
+test('Add 3 + 5', t => {
+  const result = library.add(3, 5)
+
+  t.is(result, 8, 'Output should be equal to 8')
+})
+
+// test('automatic pass', t => t.pass())
+// test('automatic pass', t => t.fail())
+
